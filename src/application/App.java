9ef8d76eef20159e26238fe8cd3a6f9ea1d25370
earl-stephens <stephensearl@hotmail.com>
2021-12-23 22:46:32 -0700

    Complete coding challenge

--- src/application/App.java
+++ src/application/App.java
@@ -3,8 +3,33 @@ package application;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/* Coding challenge
+		 * Create a multiplication table, like this:
+		 * 
+		 * 1 2 3 4 ..... 12
+		 * 2 4 6 8 ..... 24
+		 * .....
+		 * .....
+		 * 12 24 36 48 ..... 144
+		 */
+		int sizeOfArray = 12;
+		
+		int[][] outerArray = null;
+		outerArray = new int[sizeOfArray][sizeOfArray];
+		
+		for(int i = 0; i < sizeOfArray; i++) {
+			for(int j= 0; j < sizeOfArray; j++) {
+				outerArray[i][j] = (i + 1) * (j + 1);
+			}
+		}
+		
+		for(int k = 0; k < outerArray.length; k++) {
+			for(int m = 0; m < outerArray[k].length; m++) {
+				System.out.printf("%s\t", outerArray[k][m]);
+			}
+			System.out.println();
+		}
+		
 	}
 
 }
